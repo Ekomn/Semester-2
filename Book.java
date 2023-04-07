4a364d2c4f31514145872baa962f6e37d46a4735
Ekomn <121342104+Ekomn@users.noreply.github.com>
2023-04-07 21:33:18 +0700

    Add files via upload
    Tugas sesi 6 TI22B Eko Memen Nuryaman

---
+++ Book.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+    
+    // tugas sesi 6 eko memen nuryaman
+
+class Book {
+    public String judul, author, penerbit;
+    public Integer kategori, tahun;
+    private ArrayList<String> listKategori = new ArrayList<String>();
+    public Book(){
+        this.listKategori.add(0, "Fiksi");
+        this.listKategori.add(1, "Teknik");
+        this.listKategori.add(2, "Manajemen");
+        this.listKategori.add(3, "Lawas");
+        this.listKategori.add(4, "Lainnya");
+    }
+
+    public Boolean checkAvailableCategory() {
+        Boolean isFound = false;
+        if (this.kategori >= 0 && this.kategori < this.listKategori.size()) {
+            isFound = true;
+        }
+        return  isFound;
+    }
+
+    public String getKategori() {
+        return this.listKategori.get(this.kategori);
+    }
+
+    public ArrayList<String> getListKategori() {
+        return this.listKategori;
+    }
+
+}
